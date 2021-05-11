dba9ac371c65ec51818ba42a5411e048427b25b3
Daeki <kimdaeki@gmail.com>
2021-05-11 16:49:11 +0900

    1

--- src/main/java/com/iu/s1/board/notice/NoticeController.java
+++ src/main/java/com/iu/s1/board/notice/NoticeController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.iu.s1.board.BoardVO;
 
@@ -31,6 +32,15 @@ public class NoticeController {
 		List<BoardVO> ar = noticeService.getList();
 		model.addAttribute("list", ar);
 		return "board/list";
+	}
+	
+	@GetMapping("select")
+	public ModelAndView getSelect(BoardVO boardVO)throws Exception{
+		ModelAndView mv = new ModelAndView();
+		boardVO = noticeService.getSelect(boardVO);
+		mv.addObject("vo", boardVO);
+		mv.setViewName("board/select");
+		return mv;
 	}
 	
 
