864214f4ac5d1e84f2acc94383b1d97d5609b0c9
Daeki <kimdaeki@gmail.com>
2021-05-12 14:15:37 +0900

    1

--- src/main/java/com/iu/s1/util/Pager.java
+++ src/main/java/com/iu/s1/util/Pager.java
@@ -7,6 +7,54 @@ public class Pager {
 	
 	private Long startRow;
 	
+	//page
+	private Long startNum;
+	private Long lastNum;
+	
+	private boolean pre;
+	private boolean next;
+	
+	public void makeNum(Long totalCount) {
+		int perBlock=5;
+		//1. totalCount
+		
+		//2. totalCount를 이용해서 totalPage수 구하기
+		Long totalPage = totalCount/this.getPerPage();
+		if(totalCount%this.getCurPage() != 0) {
+			totalPage++;
+		}
+		//3. totalPage를 이용해서 totalBlock 수 구하기
+		Long totalBlock = totalPage / perBlock;
+		if(totalPage%perBlock !=0) {
+			totalBlock++;
+		}
+		
+		//4. curPage를 이용해서 curBlock 구하기
+		Long curBlock = this.getCurPage() / perBlock;
+		if(this.curPage%perBlock !=0) {
+			curBlock++;
+		}
+		
+		//5. curBlock를 이용해서 startNum, lastNum 구하기
+		Long startNum=(curBlock-1)*perBlock+1;
+		Long lastNum=curBlock*perBlock;
+		
+		//6. curBlock이 마지막(totalBlock)
+		this.pre=true;
+		this.next=true;
+		if(curBlock==totalBlock) {
+			lastNum = totalPage;
+			this.next=false;
+		}
+		
+		if(curBlock==1) {
+			this.pre=false;
+		}
+		
+		
+		
+	}
+	
 	public void makeRow() {
 		// curPage 	 	startRow
 		// 1			0
@@ -51,6 +99,38 @@ public class Pager {
 
 	public void setStartRow(Long startRow) {
 		this.startRow = startRow;
+	}
+
+	public Long getStartNum() {
+		return startNum;
+	}
+
+	public void setStartNum(Long startNum) {
+		this.startNum = startNum;
+	}
+
+	public Long getLastNum() {
+		return lastNum;
+	}
+
+	public void setLastNum(Long lastNum) {
+		this.lastNum = lastNum;
+	}
+
+	public boolean isPre() {
+		return pre;
+	}
+
+	public void setPre(boolean pre) {
+		this.pre = pre;
+	}
+
+	public boolean isNext() {
+		return next;
+	}
+
+	public void setNext(boolean next) {
+		this.next = next;
 	}
 	
 	
