76f5daaaabe3943175d17be75e04613d8210aea8
Daeki <kimdaeki@gmail.com>
2021-05-17 14:51:27 +0900

    1

--- src/main/java/com/iu/s1/member/MemberController.java
+++ src/main/java/com/iu/s1/member/MemberController.java
@@ -3,7 +3,9 @@ package com.iu.s1.member;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping("/member/**")
@@ -19,5 +21,11 @@ public class MemberController {
 	@GetMapping("join")
 	public String setJoin()throws Exception{
 		return "member/memberJoin";
+	}
+	@PostMapping("join")
+	public String setJoin(MemberVO memberVO, MultipartFile [] avatar)throws Exception{
+		int result = memberService.setJoin(memberVO, avatar);
+		
+		return "redirect:../";
 	}
 }
